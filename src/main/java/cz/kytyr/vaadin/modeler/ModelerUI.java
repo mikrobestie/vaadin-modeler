82b29764bfe054c13eab791cb04a2ff8dc3b670a
Michal <michal.kytyr@gmail.com>
2015-09-23 21:22:01 +0200

    first commit

---
+++ src/main/java/cz/kytyr/vaadin/modeler/ModelerUI.java
@@ -0,0 +1,24 @@
+/*
+ * encoding="UTF-8", (greek letters ro psi: ρψ). Do not change this comment!!!
+ * Copyright 2014 © Syntea software group a.s.
+ */
+package cz.kytyr.vaadin.modeler;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+/**
+ * Main UI.
+ *
+ * @author mikrobestie
+ * @since 23.9.2015
+ */
+@SpringUI
+public class ModelerUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
---
+++ src/main/java/cz/kytyr/vaadin/modeler/VaadinModelerApplication.java
@@ -0,0 +1,12 @@
+package cz.kytyr.vaadin.modeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinModelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinModelerApplication.class, args);
+    }
+}
