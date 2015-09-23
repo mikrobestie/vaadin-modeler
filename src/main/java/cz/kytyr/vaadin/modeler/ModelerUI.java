bf8f625461a3f51fa147588b8b29f40e9b4832cd
Michal <michal.kytyr@gmail.com>
2015-09-24 00:07:10 +0200

    first commit

--- src/main/java/cz/kytyr/vaadin/modeler/ModelerUI.java
+++ src/main/java/cz/kytyr/vaadin/modeler/ModelerUI.java
@@ -4,9 +4,17 @@
  */
 package cz.kytyr.vaadin.modeler;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.navigator.SpringViewProvider;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
+import cz.kytyr.vaadin.modeler.component.SideBar;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Main UI.
@@ -15,10 +23,34 @@ import com.vaadin.ui.UI;
  * @since 23.9.2015
  */
 @SpringUI
+@Theme("modeler")
 public class ModelerUI extends UI {
+
+    // we can use either constructor autowiring or field autowiring
+    @Autowired
+    private SpringViewProvider viewProvider;
+
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        final HorizontalLayout root = new HorizontalLayout();
+        root.setSizeFull();
+        setContent(root);
 
+        final Panel viewContainer = new Panel();
+        viewContainer.setSizeFull();
+
+        HorizontalLayout centerContentLayout = new HorizontalLayout();
+        centerContentLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        centerContentLayout.setPrimaryStyleName("centered-content");
+        centerContentLayout.setSizeFull();
+        viewContainer.setContent(centerContentLayout);
+
+        root.addComponent(viewContainer);
+        root.addComponent(new SideBar());
+        root.setExpandRatio(viewContainer, 1.0f);
+
+        Navigator navigator = new Navigator(this, centerContentLayout);
+        navigator.addProvider(viewProvider);
     }
 }
---
+++ src/main/java/cz/kytyr/vaadin/modeler/view/CanvasView.java
@@ -0,0 +1,83 @@
+/*
+ * encoding="UTF-8", (greek letters ro psi: ρψ). Do not change this comment!!!
+ * Copyright 2014 © Syntea software group a.s.
+ */
+package cz.kytyr.vaadin.modeler.view;
+
+import com.vaadin.event.LayoutEvents;
+import com.vaadin.event.dd.DragAndDropEvent;
+import com.vaadin.event.dd.DropHandler;
+import com.vaadin.event.dd.acceptcriteria.AcceptAll;
+import com.vaadin.event.dd.acceptcriteria.AcceptCriterion;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.DragAndDropWrapper;
+import com.vaadin.ui.Label;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Michal
+ * @since 23.9.2015
+ */
+@SpringView(name = "")
+public class CanvasView extends DragAndDropWrapper implements View {
+
+    public static final String STYLE_SELECTABLE = "_selectable";
+    public static final String STYLE_SELECTED = "_selected";
+
+    private CssLayout layout;
+    private Component selected;
+
+
+    public CanvasView() {
+        super(new CssLayout());
+    }
+
+    @PostConstruct
+    void init() {
+        setPrimaryStyleName("canvas");
+        setWidth(640, Unit.PIXELS);
+        setHeight(480, Unit.PIXELS);
+
+        layout = (CssLayout) getCompositionRoot();
+        layout.addLayoutClickListener(this::onComponentSelected);
+
+        setDropHandler(new DropHandler() {
+
+            @Override
+            public void drop(DragAndDropEvent event) {
+
+                Component sourceComponent = event.getTransferable().getSourceComponent();
+                layout.addComponent(sourceComponent);
+                sourceComponent.addStyleName(STYLE_SELECTABLE);
+            }
+
+            @Override
+            public AcceptCriterion getAcceptCriterion() {
+                return AcceptAll.get();
+            }
+        });
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+
+        Label label = new Label("Hello world!");
+        label.addStyleName(STYLE_SELECTABLE);
+        layout.addComponent(label);
+    }
+
+    private void onComponentSelected(LayoutEvents.LayoutClickEvent e) {
+        if (e.getClickedComponent() != selected) {
+            if (selected != null) {
+                selected.removeStyleName(STYLE_SELECTED);
+            }
+            selected = e.getClickedComponent();
+            selected.addStyleName(STYLE_SELECTED);
+        }
+    }
+}
